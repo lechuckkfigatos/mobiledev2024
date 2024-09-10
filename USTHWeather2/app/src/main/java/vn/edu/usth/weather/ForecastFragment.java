57d086d03582c99f875534e165b67938dedcb75e
Ha Le Hoang <hoanghl.22bi13170@usth.edu.vn>
2024-09-10 10:39:15 +0700

    Ex6: Relative Layout

--- USTHWeather2/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ USTHWeather2/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -1,14 +1,10 @@
 package vn.edu.usth.weather;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+
 import androidx.fragment.app.Fragment;
 
 public class ForecastFragment extends Fragment {
